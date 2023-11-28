be8f396adcbe476c3a08c8ef35d6c9fc76f924d3
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-11-28 05:28:53 +0500

    Update progress1(rayyan).c

--- Progress1/progress1(rayyan).c
+++ Progress1/progress1(rayyan).c
@@ -34,7 +34,7 @@ int calculatePrice(struct Transportation *transport, bool isPeakTime)
 
     return finalPrice;
 }
-
+/*
 void generateBill(char userName[20], char driverName[20], float finalPrice, char pickupLocation[50], char dropoffLocation[50]) 
 {
     printf("\n---------------------- Ride Hailing Bill -----------------------\n\n");
@@ -45,6 +45,76 @@ void generateBill(char userName[20], char driverName[20], float finalPrice, char
     printf("Final Price: $%.2f\n", finalPrice);
     printf("\n----------------------------------------------------------------\n");
 }
+*/
+
+void simulatePaymentProcessing(float finalPrice, int paymentMethod) 
+{
+    switch (paymentMethod) 
+    {
+        case 1:
+            printf("Payment successful! You have paid with cash.\n");
+            break;
+        case 2:
+            printf("Payment successful! You have paid with a debit card.\n");
+            break;
+        case 3:
+            printf("Payment successful! You have paid with a credit card.\n");
+            break;
+        case 4:
+            printf("Payment successful! You have paid with Easypaisa.\n");
+            break;
+        default:
+            printf("Invalid payment method. Exiting...\n");
+            exit(1);
+    }
+}
+
+
+void rateDriver(struct CabDriver *driver) 
+{
+    int rating;
+    printf("Rate your driver (1 to 5): ");
+    scanf("%d", &rating);
+
+    if (rating >= 1 && rating <= 5) 
+    {
+        driver->rating = (driver->rating + rating) / 2.0; // Calculate average rating
+        printf("Thank you for your rating!\n");
+    } else 
+    {
+        printf("Invalid rating. Please enter a number between 1 and 5.\n");
+        rateDriver(driver); // Recursively ask for a valid rating
+    }
+}
+
+
+// Enhanced function to generate the bill
+void generateBill(struct User user, struct CabDriver driver, struct Transportation transport, float finalPrice) {
+    printf("\n---------------------- Ride Hailing Bill -----------------------\n\n");
+    
+    printf("User Details:\n");
+    printf("Name: %s\n", user.name);
+    printf("Phone Number: %s\n", user.phoneNumber);
+    printf("Contact Details: %s\n", user.contactDetails);
+
+    printf("\nDriver Details:\n");
+    printf("Name: %s\n", driver.name);
+    printf("Phone Number: %s\n", driver.phoneNumber);
+    printf("Email: %s\n", driver.email);
+    printf("Cab Number: %s\n", driver.cabNumber);
+    printf("Rating: %.2f\n", driver.rating);
+
+    printf("\nRide Details:\n");
+    printf("Destination: %s\n", transport.destination);
+    printf("Vehicle Type: %s\n", transport.vehicleType);
+    printf("Base Price: $%.2f\n", transport.basePrice);
+    printf("Peak Factor: %.2f\n", transport.peakFactor);
+    printf("Final Price: $%.2f\n", finalPrice);
+
+    printf("\n----------------------------------------------------------------\n");
+}
+
+
 
 int main() 
 {
@@ -74,7 +144,12 @@ int main()
     printf("Enter driver's name: ");
     scanf("%s", driverName);
 
-    generateBill(userName, driverName, finalPrice, userDestination, userVehicleType);
+    //generateBill(userName, driverName, finalPrice, userDestination, userVehicleType);
+    generateBill(userDetails, chosenDriver, transportDetails, finalPrice);
+    
+    simulatePaymentProcessing(finalPrice, userDetails.paymentMethod);
+
+    rateDriver(&chosenDriver);
 
     return 0;
 }
