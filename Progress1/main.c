eb8c0edbb07e955ab736cc95e94f7771210600cb
iamrayyan1 <142688529+iamrayyan1@users.noreply.github.com>
2023-12-01 04:25:08 +0500

    Update main.c

--- Progress1/main.c
+++ Progress1/main.c
@@ -1,1 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+int main() 
+{
+    // User details
+    struct User userDetails;
+
+    // Get user details, including registration status
+    getUserDetails(&userDetails);
+
+    // Check if the user is registered
+    if (!userDetails.isRegistered) 
+    {
+        printf("You are not registered for transportation yet.\n");
+        // Handle the registration process
+        registerUser(&userDetails);
+    } else 
+    {
+        printf("You are already registered for transportation.\n");
+    }
+
+    // Check for peak time
+    int isPeakTimeFlag = isPeakTime();
+
+    // Transportation details
+    struct Transportation transportDetails;
+    initializeTransportation(&transportDetails);
+
+    // User input for destination and vehicle type
+    char userDestination[50];
+    char userVehicleType[20];
+
+    printf("Enter your destination: ");
+    scanf("%s", userDestination);
+
+    printf("Choose your preferred vehicle (Bike, Car, Rickshaw): ");
+    scanf("%s", userVehicleType);
+
+    // Calculate final price
+    float finalPrice = calculatePrice(&transportDetails, isPeakTimeFlag);
+
+    // Cab driver details
+    struct CabDriver chosenDriver;
+    // Select a driver based on some criteria
+    selectDriver(&chosenDriver);
+
+    // Generate and display the bill
+    generateBill(userDetails, chosenDriver, transportDetails, finalPrice);
+
+    // Simulate payment processing
+    simulatePaymentProcessing(finalPrice, userDetails.paymentMethod);
+
+    // Rate the driver
+    rateDriver(&chosenDriver);
+
+    // Additional functionality for the user experience
+    int userExperienceChoice;
+    printf("Would you like to provide feedback on your experience? (1 for Yes / 0 for No): ");
+    scanf("%d", &userExperienceChoice);
+
+    if (userExperienceChoice) 
+    {
+        provideUserFeedback(&userDetails);
+    }
+
+    return 0;
+}
